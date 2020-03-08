5ffb590e5913c41ac614eea737bf48bb152b4750
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-03-08 20:31:11 +0300

    Fix repo in delivery script

--- fetch_latest_artifact.py
+++ fetch_latest_artifact.py
@@ -3,7 +3,7 @@ import urllib.request
 import os
 
 
-build_info_url = 'https://circleci.com/api/v1.1/project/github/ahlinist/raspberry-pi4j/latest/artifacts'
+build_info_url = 'https://circleci.com/api/v1.1/project/github/ahlinist/bobby/latest/artifacts'
 build_info = requests.get(url=build_info_url)
 build_info_json = build_info.json()
 artifact_url = build_info_json[0].get('url')

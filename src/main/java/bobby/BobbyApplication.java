b5699786938d8c7ed5082018fcc1237366a2bbce
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-11-11 23:51:33 +0300

    Remove camera lib

--- src/main/java/bobby/BobbyApplication.java
+++ src/main/java/bobby/BobbyApplication.java
@@ -8,14 +8,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import uk.co.caprica.picam.Camera;
-import uk.co.caprica.picam.CameraConfiguration;
-import uk.co.caprica.picam.FilePictureCaptureHandler;
-import uk.co.caprica.picam.enums.Encoding;
-
-import java.io.File;
-
-import static uk.co.caprica.picam.PicamNativeLibrary.installTempLibrary;
 
 @Slf4j
 @SpringBootApplication
@@ -30,30 +22,6 @@ public class BobbyApplication implements ApplicationRunner {
 	@Override
 	@SneakyThrows
 	public void run(ApplicationArguments args) {
-		camera();
 		log.info("Starting the bobby app...");
-	}
-
-	@SneakyThrows
-	private void camera() {
-		try {
-			installTempLibrary();
-		} catch(Exception e) {
-			System.out.println("Failed to load camera library");
-			e.printStackTrace();
-		}
-
-		CameraConfiguration config = CameraConfiguration.cameraConfiguration()
-				.width(1920)
-				.height(1080)
-				.encoding(Encoding.JPEG)
-				.quality(85);
-
-		try (Camera camera = new Camera(config)) {
-			camera.takePicture(new FilePictureCaptureHandler(new File("picam1.jpg")));
-			camera.takePicture(new FilePictureCaptureHandler(new File("picam2.jpg")));
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
 	}
 }

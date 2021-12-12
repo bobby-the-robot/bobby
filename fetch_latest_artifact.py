79302a7b07fb13f64448637657b1f53012bce90d
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-12-12 20:56:55 +0300

    Update fetch artifact script

--- fetch_latest_artifact.py
+++ fetch_latest_artifact.py
@@ -1,11 +1,24 @@
 import requests
 import urllib.request
+import zipfile
 import os
 
+if os.path.exists('./bobby.zip'):
+  os.remove('./bobby.zip')
+if os.path.exists('./bobby.jar'):
+  os.remove('./bobby.jar')
 
-build_info_url = 'https://circleci.com/api/v1.1/project/github/ahlinist/bobby/latest/artifacts'
-build_info = requests.get(url=build_info_url)
-build_info_json = build_info.json()
-artifact_url = build_info_json[0].get('url')
-urllib.request.urlretrieve(artifact_url, 'bobby.jar')
+#requires 'token' file to contain github personal token with public repo access
+github_token = open('./token','r').read().rstrip('\n')
+
+artifacts_url = 'https://api.github.com/repos/ahlinist/bobby/actions/artifacts'
+artifacts = requests.get(url=artifacts_url)
+artifacts_json = artifacts.json()
+artifacts_array = artifacts_json['artifacts']
+latest_artifact = artifacts_array[0]
+zip_url = artifacts_url + '/' + str(latest_artifact['id']) + '/zip'
+zip_url_response = requests.get(url=zip_url, headers={'Authorization': 'token ' + github_token})
+open('./bobby.zip', 'wb').write(zip_url_response.content)
+with zipfile.ZipFile('./bobby.zip', 'r') as zip_ref:
+    zip_ref.extractall('.')
 os.system('java -jar bobby.jar')
